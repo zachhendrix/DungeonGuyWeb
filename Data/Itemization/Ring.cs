a43f42a8918bc78183fa85d94fa7c01c8d722126
Zachariah Hendrix <Zach.Hendrix@tyson.com>
2025-09-08 14:51:08 -0500

    Refactor to match Web

--- Data/Itemization/Ring.cs
+++ Data/Itemization/Ring.cs
@@ -5,8 +5,8 @@ namespace Data.Itemization
     public class Ring(string name, string description, float weight, int baseValue, Equipment.Slot slot)
                : ItemObject(name, description, weight, baseValue, ItemType.Ring)
     {
-        public string Effect { get; set; } // e.g., "Healing", "Mana Restoration", etc.
-        public int Duration { get; set; } // Duration in seconds
+        public required string Effect { get; set; }
+        public int Duration { get; set; }
         public Equipment.Slot Slot { get; set; } = slot;
     }
 }
--- Data/Itemization/Treasure.cs
+++ Data/Itemization/Treasure.cs
@@ -2,8 +2,6 @@
 {
     public class Treasure : ItemObject
     {
-
-
         public Treasure(string name, string description, float weight, int baseValue)
             : base(name, description, weight, baseValue, ItemType.Treasure)
         {
